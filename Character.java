f61c4f75d3a0d29aaed88160a2f2e2789cde6794
Charlie Su <dragonsmirth@gmail.com>
2014-04-19 17:35:23 -0700

    Updated Character.java and Dungeon.java
    Character.java = added fields
    Dungeon.java = added fields, constructor, and currently working on
    methods

---
+++ Character.java
@@ -0,0 +1,39 @@
+/*
+Character.java is the superclass of NPC.java, Player.java, and Enemy.java. All characters will have the following attributes in the file.
+1] Loading Sprite (not sure about animation)
+2] HP
+3] Max HP
+4] X and Y coordinate positions (in the map)
+5] Inventory
+6] Skills
+*/
+public class Character extends JFrame
+{
+//Note to self: Remember to update public or not based on how you want other classes to access this file
+	//Field
+	BufferedImage image; //Creates an image and loads it. 
+	int maxHealth;//maximum HP
+	int currentHealth; //current HP
+	double xPos; //current x-coordinate position
+	double yPos; //current y-coordinate position
+	ArrayList<Integer> inventory = new ArrayList<Integer>;//this is the inventory
+	Skill[] skill = new Skill[20];//maximum number of skills you can hold is 20.
+	public int spriteHeight;//height of image, so rescaling is possible
+	public int spriteWidth;//width of image, so rescaling is possible
+	//Constructor
+	public Character(int maxHealth)
+	{
+		this.maxHealth = maxHealth;
+	}
+	//Superclass's Methods
+   public void draw(Graphics g)
+   {
+      g.drawImage(image, XPos, YPos, XSpriteSize, YSpriteSize, null);
+   }
+   
+   public void paint(Graphics g)
+   {
+      ObjectProperites.super.paint(g);
+   }
+   //Methods unique to Character.java
+}
--- Dungeon.java
+++ Dungeon.java
@@ -1,35 +1,113 @@
-class Dungeon
+/*
+DungeonRunner.java is the file that generates the dungeon based on the information included in NodeWorld.java
+DungeonRunner.java will tell LegacyDungeon.java (the main runner) what to display on the screen, so yeah.
+*/
+
+public class DungeonRunner
 {
-<<<<<<< HEAD
-//Fields
-int numMonsters //How many monsters there are are
-//Each enemy has a difficulty rating. We randomly generate monsters to match this difficulty
-int currentFloor
+	//Fields
+	int numMonsters; //# of monsters
+	int currentFloor; //keeps track of floor number
+	int numFloor; //how many floors are there?
+	int theme;//theme of the dungeon
+	int skillID;//skill reward at bottom of dungeon
+	int difficulty;//difficulty of dungeon
+	//Constructor
+	public DungeonRunner(int theme, int skillID, int difficulty)//Takes in the following parameters from NodeWorld.java
+	{
+		this.theme = theme;
+		this.skillID = skillID;
+		this.difficulty = difficulty;
+		currentFloor = 1;
+		numFloor = (int) (Math.random()*50) + 1;
+	}
+//////////////////////////////////METHODS HERE///////////////////////////////////////
+/*
+List of methods:
+1] What happens when I reach the stairs?
+Answer: You will move to a new map
+	a] What happens when I reach the last floor?
+Answer: A unique map will be generated
 
+2] How many monsters will be spawned and where will they be?
+Answer: Random # of monsters and random positions
 
+3] What tiles should be loaded?
+Answer: Depends on the theme based on the node's data.
+	How many tiles should be loaded?
+Answer: Random #
 
-   public void main(Theme theme, int xLength, int yLength, int floorNum, int difficulty)
-      {
-         
+4] Where will I be spawned?
+Answer: Random position
+
+5] Where IS the stairs?
+Answer: Random position set as far from you as possible
+
+Method 0: .checkStairs()
+Method 1: .checkLastFloor()
+Method 2: .monsterGenerator()
+Method 3: .loadTiles()
+Method 4: .spawnPlayer()
+Method 5: .spawnStairs()
+*/
+	//Method 0: .checkStairs() checks whether or not the player reached the stairs
+	public static boolean checkStairs(boolean reachStairs)
+	{
+		if(reachStairs)
+		{
+			currentFloor++;
 		}
-=======
-    public void main(Theme theme, array dungeonStats, )
-        {
-            generateFloor(dungeonStats[0], dungeonStats[1])
+	}
+	//Method 1: .checkLastFloor() checks whether or not the player will is at the last floor
+	public static boolean checkLastFloor(int currentFloor)
+	{
+		currentFloor = this.currentFloor;
+		if(currentFloor == numFloor)
+		{
+			
 		}
-      
-    public static void nextFloor()
-        {
-   
-        }
-
-        //The generated floor will use x and y coordinates, number of monsters, and 
-    public static void generateFloor(
-    {
-    
-    
-    }
-      
-      
->>>>>>> 8705cc9f0f522554fa84054477e218d11f491631
+	//Method 2: .monsterGenerator() generates monsters and gives them a position
+	//Method 3: .loadTiles() chooses the picture tile to load and draws out the map
+	//Method 4: .spawnPlayer() spawns the player at a random position
+	//Method 5: .spawnStairs() spawns stairs at a random position significantly far away from the player
+	
+//This is the method that checks whether or not you've entered another floor
+	public static boolean newFloor(boolean reachStairs)
+	{
+		if(reachStairs)
+		{
+			currentFloor++;
+			if(currentFloor = numFloor)
+			{
+				return true;//we have reached the end of the dungeon
+			}
+		}
+		//Generate a new map
+		numMonsters = (int)(Math.random());
+		return false;//we have not reached the end of the dungeon. LegacyDungeon will generate a new map with the current variables
+	}
+	//Generates monsters
+	public static void monsterGenerator(int numMonsters)
+	{
+		Monster[] monsters = new Monster[numMonsters]; 
+		for(int i = numMonsters; i > 0; i--)
+		{
+			monsters[i] = new Monster();//will insert parameters in here later
+		}
+	}
+	//Checks if Item and Character are in the same box
+	public static void onSameTile(Object firstObject, Object secondObject)
+	{
+		if(firstObject instanceof Character && secondObject instanceof Item)
+		{
+			if(firstObject.singularity || secondObject.singularity)
+			{
+				return false;
+			}
+			return true;
+		}
+		return false;
+	}
+	//
+	
 }
