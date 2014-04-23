75f33b226e64e264d4935665f40513f1b81df17f
anikan <anikan33@gmail.com>
2014-04-22 18:23:06 -0700

    Working tile generation
    Protip: make sure to have your while loop variable set to true
    initially.

--- DungeonRunner.java
+++ DungeonRunner.java
@@ -10,32 +10,32 @@ DungeonRunner.java will tell LegacyDungeon.java (the main runner) what to displa
 //skills level can increase by # of monsters
 public class DungeonRunner
 {
-	//Fields
-	int numMonsters; //# of monsters
-	int currentFloor; //keeps track of floor number
-	int numFloor; //how many floors are there?
-	int theme;//theme of the dungeon
-	int skillID;//skill reward at bottom of dungeon
-	int difficulty;//difficulty of dungeon
+   //Fields
+   int numMonsters; //# of monsters
+   int currentFloor; //keeps track of floor number
+   int numFloor; //how many floors are there?
+   int theme;//theme of the dungeon
+   int skillID;//skill reward at bottom of dungeon
+   int difficulty;//difficulty of dungeon
     static int xLengthInput;
     static int yLengthInput;
     static int numTiles;
     public static ArrayList<DungeonTile> tileList = new ArrayList<DungeonTile>();
     public static ArrayList<DungeonTile> connectorList = new ArrayList<DungeonTile>();
-	//Constructor
-	public DungeonRunner(int theme, int skillID, int difficulty)//Takes in the following parameters from NodeWorld.java
-	{
-		this.theme = theme;
-		this.skillID = skillID;
-		this.difficulty = difficulty;//insert random factor that will adjust difficulty
-		currentFloor = 1;
-		numFloor = (int) (Math.random()*difficulty) + (int)(difficulty/10) + 1;
+   //Constructor
+   public DungeonRunner(int theme, int skillID, int difficulty)//Takes in the following parameters from NodeWorld.java
+   {
+      this.theme = theme;
+      this.skillID = skillID;
+      this.difficulty = difficulty;//insert random factor that will adjust difficulty
+      currentFloor = 1;
+      numFloor = (int) (Math.random()*difficulty) + (int)(difficulty/10) + 1;
         //Remove this stuff later. Just for testing.
         xLengthInput = 50;
         yLengthInput = 50;
         numTiles = 50;
-		//Number of floors is based on the difficulty level
-	}
+      //Number of floors is based on the difficulty level
+   }
 //////////////////////////////////METHODS HERE///////////////////////////////////////
 /* List of Methods:
 Method 0: .checkSpace() runs every time the character moves. It makes sure the player doesn't sit on the same tile as a monster.
@@ -65,37 +65,38 @@ Method 8: .checkAtBorder() runs every time the character moves. It makes sure th
     //Charlie, please make this less ugly. This generates the floor tiles of the dungeon. REMOVE STATIC WHEN WORKING.
     public void assignTilePos(int xLength, int yLength, int numTiles)
     {
-        //The first seed tile.
-        tileList.add(0, new DungeonTile(xLength/2, yLength/2, 1));
-        connectorList.add(0, new DungeonTile(xLength/2, yLength/2, 1));
-
+        //The first seed tile. Perhaps have a random seed tile?
+        tileList.add(0, new DungeonTile((int)xLength/2, (int)yLength/2, 0));
+        connectorList.add(0, new DungeonTile((int)xLength/2, (int)yLength/2, 0));
         //Starting at 1 because seed is 0.
         for (int i = 1; i < numTiles; i++)
         {
             //Methods needed: get adjacent tile, check if good tile, add to tileList and connectorList, remove tiles from connector list with more than connectionCap connections
-            boolean boolGoodTile = false;
+            boolean boolGoodTile = true;
             int connectionCap = 2;
             int pickedTileNumber = 0;
-            DungeonTile possibleTile = new DungeonTile(0,0,0);
+            DungeonTile possibleTile = new DungeonTile(0,1,0);
+            
             while (boolGoodTile)
             {
-            pickedTileNumber = pickConnectorTile();
-            //Gets the index of a random connector from the connectorList.
-            //Gets a random tile adjacent to picked connector.
-            possibleTile = getAdjacentTile(pickedTileNumber);
-            //If picked tile is invalid, then restart with another connector.
-            boolGoodTile = checkTileSpace(possibleTile, xLength, yLength);            
+                pickedTileNumber = pickConnectorTile();
+                //Gets the index of a random connector from the connectorList.
+                //Gets a random tile adjacent to picked connector.
+                possibleTile = getAdjacentTile(pickedTileNumber);
+                //If picked tile is invalid, then restart with another connector.
+                boolGoodTile = checkTileSpace(possibleTile, xLength, yLength);            
             }
+            
             //Add a connection to the picked tile.
             connectorList.get(pickedTileNumber).numConnections += 1;
-            tileList.add(i, possibleTile);
-            connectorList.add(i, possibleTile);
+            tileList.add(possibleTile);
+            connectorList.add(possibleTile);
             //Checks if the latest connector reaches the max number of connections and deletes it if it has.
             if (connectorList.get(pickedTileNumber).numConnections >= connectionCap)
             {
                 connectorList.remove(pickedTileNumber);
             }
-            
+            System.out.println(possibleTile.x + " " + possibleTile.y);
         }    
     }
     
@@ -103,7 +104,7 @@ Method 8: .checkAtBorder() runs every time the character moves. It makes sure th
     private int pickConnectorTile()
     {
         //Pick a random tile from the connector list. Please check if this will work. 
-        int tileChooser = (int) Math.random() * connectorList.size();
+        int tileChooser = (int)(Math.random() * connectorList.size());
         return tileChooser;
     }
 
@@ -111,8 +112,7 @@ Method 8: .checkAtBorder() runs every time the character moves. It makes sure th
     private DungeonTile getAdjacentTile(int tileChooser)
     {
         int x;
-        int y;
-        
+        int y;       
         if (Math.random() >= .5)
         {
             x = (connectorList.get(tileChooser).x + 1);
@@ -151,18 +151,17 @@ Method 8: .checkAtBorder() runs every time the character moves. It makes sure th
                 
                 if (DiffX == 0 && DiffY == 0)
                 {
-                    return false;                
+                    return true;                
                 }                            
             }            
         
         if (tile.x == 0 || tile.x == xLength || tile.y == 0 || tile.y == yLength)
         {
-            return false;
+            return true;
         }
-        
         else
         {
-            return true;  
+            return false;  
         }
     }
     /*
@@ -223,84 +222,84 @@ Method 8: .checkAtBorder() runs every time the character moves. It makes sure th
     }
     */
 /*
-	//Method 1: .checkStairs() checks whether or not the player reached the stairs. Note that on LegacyDungeon.java, it will run this method first, and if it returns true, it will run every other method.
-	public static boolean checkStairs(boolean reachStairs)
-	{
-		if(reachStairs)
-		{
-			currentFloor++;
-			return true;
-		}
-		return false;
-	}
-	//Method 2: .checkLastFloor() checks whether or not the player will is at the last floor
-	public static boolean checkLastFloor(int currentFloor)
-	{
-		currentFloor = this.currentFloor;
-		if(currentFloor == numFloor)
-		{
-			return true;
-		}
-		return false;
-	}
-	//Method 4: .monsterGenerator() generates monsters and gives them a position
-	public static void monsterGenerator()
-	{
-		numMonsters = (int)(Math.random()*difficulty);//generates a random number of monsters based on difficulty
-		Monster[] monsters = new Monster[numMonsters];//creates an array of monsters, and initializes each monster.
-		for(int i = numMonsters; i > 0; i--)
-		{
-			monsters[i] = new Monster();//will insert Monster parameter in here later
-			monsters[i].xPos =;
-			monsters[i].yPos =;
-		}
-	}
-	//Method 3a: .checkSpace() checks if Item and Character are in the same box
-	public static void onSameTile(Object firstObject, Object secondObject)
-	{
-		if(firstObject instanceof Character && secondObject instanceof Item)
-		{
-			if(firstObject.singularity || secondObject.singularity)
-			{
-				return false;
-			}
-			return true;
-		}
-		return false;
-	}
-	//Method 3b: .loadFloor() chooses the picture tile to load and draws out the actual map
-	public static void loadFloor()
-	{
-		//check if last floor, if not, continue
-		if(currentFloor == numFloor)
-		{
-			return;
-		}
-		//determine 
-	}
-	//Method 4: .spawnPlayer() spawns the player at a random position
-	public static void spawnPlayer()
-	{
-		//load sprite
-		//generate random coordinates
-		//check if random coordinates are valid (within bounds of loadFloor)
-		//if not regenerate
-		//if so generate
-	}
-	//Method 5: .spawnStairs() spawns stairs at a random position significantly far away from the player
-	public static void spawnStairs()
-	{
-		//check if last floor. If last floor, don't do anything
-		//load sprite
-		//generate random coordinates
-		//check if within bounds and not near player
-		//generate
-	}
-	//Method 6: .loadLastFloor() loads the last floor pre-determined map and an Item
-	public static void loadLastFloor()
-	{
-		//check if last floor. IF last floor
-		//load sprites for tile
-		//load 
-	}	*/
+   //Method 1: .checkStairs() checks whether or not the player reached the stairs. Note that on LegacyDungeon.java, it will run this method first, and if it returns true, it will run every other method.
+   public static boolean checkStairs(boolean reachStairs)
+   {
+      if(reachStairs)
+      {
+         currentFloor++;
+         return true;
+      }
+      return false;
+   }
+   //Method 2: .checkLastFloor() checks whether or not the player will is at the last floor
+   public static boolean checkLastFloor(int currentFloor)
+   {
+      currentFloor = this.currentFloor;
+      if(currentFloor == numFloor)
+      {
+         return true;
+      }
+      return false;
+   }
+   //Method 4: .monsterGenerator() generates monsters and gives them a position
+   public static void monsterGenerator()
+   {
+      numMonsters = (int)(Math.random()*difficulty);//generates a random number of monsters based on difficulty
+      Monster[] monsters = new Monster[numMonsters];//creates an array of monsters, and initializes each monster.
+      for(int i = numMonsters; i > 0; i--)
+      {
+         monsters[i] = new Monster();//will insert Monster parameter in here later
+         monsters[i].xPos =;
+         monsters[i].yPos =;
+      }
+   }
+   //Method 3a: .checkSpace() checks if Item and Character are in the same box
+   public static void onSameTile(Object firstObject, Object secondObject)
+   {
+      if(firstObject instanceof Character && secondObject instanceof Item)
+      {
+         if(firstObject.singularity || secondObject.singularity)
+         {
+            return false;
+         }
+         return true;
+      }
+      return false;
+   }
+   //Method 3b: .loadFloor() chooses the picture tile to load and draws out the actual map
+   public static void loadFloor()
+   {
+      //check if last floor, if not, continue
+      if(currentFloor == numFloor)
+      {
+         return;
+      }
+      //determine 
+   }
+   //Method 4: .spawnPlayer() spawns the player at a random position
+   public static void spawnPlayer()
+   {
+      //load sprite
+      //generate random coordinates
+      //check if random coordinates are valid (within bounds of loadFloor)
+      //if not regenerate
+      //if so generate
+   }
+   //Method 5: .spawnStairs() spawns stairs at a random position significantly far away from the player
+   public static void spawnStairs()
+   {
+      //check if last floor. If last floor, don't do anything
+      //load sprite
+      //generate random coordinates
+      //check if within bounds and not near player
+      //generate
+   }
+   //Method 6: .loadLastFloor() loads the last floor pre-determined map and an Item
+   public static void loadLastFloor()
+   {
+      //check if last floor. IF last floor
+      //load sprites for tile
+      //load 
+   }  */
 }
