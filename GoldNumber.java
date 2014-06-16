6a1e0628009cecb7b7967dd13c013dff1d8dfb87
Charlie Su <dragonsmirth@gmail.com>
2014-06-15 17:17:30 -0700

    NullPointerException
    halp

--- GoldNumber.java
+++ GoldNumber.java
@@ -1,7 +1,5 @@
 //Perhaps unneeded?
 public class GoldNumber extends Number{
-
-
     public GoldNumber(int inputDamage, int xInput, int yInput)
     {
         super(inputDamage, xInput, yInput);        
