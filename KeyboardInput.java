e8ab577e691631f1704414c09fcf26d4232c20aa
Charlie Su <dragonsmirth@gmail.com>
2015-08-30 04:21:48 -0400

    Re-added keyboard input!

--- KeyboardInput.java
+++ KeyboardInput.java
@@ -1,97 +1,32 @@
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.*;
+package LegacyDungeon;
 
-//Used to get keyboard input, similar in use to imageLoader
-public class KeyboardInput implements KeyListener
+import org.lwjgl.glfw.*;
+
+import java.util.concurrent.ConcurrentHashMap;
+//java.lang.reflect 
+//make a hashtable
+//we'll have to create a new class for different contexts
+//e.g. talking, in-dungeon, in world map
+
+/*Class  aClass = ...//obtain class object
+Method method =
+    aClass.getMethod("doSomething", new Class[]{String.class});
+*/
+public class KeyboardInput extends GLFWKeyCallback
 {
-	public static boolean boolIsUp, boolIsDown, boolIsLeft, boolIsRight, boolIsAttack, boolIsInteracting, boolIsMoving, boolIs1, boolIs2, boolIs3, boolIs4, boolIs5,boolIs6, boolIs7, boolIs8, boolIs9, boolIs0, diagnostic, boolIsHeal;
-	static boolean[] actionArray = {boolIsUp, boolIsDown, boolIsLeft, boolIsRight, boolIsAttack, boolIsInteracting, boolIs1, boolIs2, boolIs3, boolIs4, boolIs5,boolIs6, boolIs7, boolIs8, boolIs9, boolIs0, diagnostic, boolIsHeal};
-	int[] keyArray = {KeyEvent.VK_UP, KeyEvent.VK_DOWN, KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT, KeyEvent.VK_SPACE, KeyEvent.VK_SHIFT, KeyEvent.VK_1, KeyEvent.VK_2, KeyEvent.VK_3, KeyEvent.VK_4, KeyEvent.VK_5, KeyEvent.VK_6, KeyEvent.VK_7, KeyEvent.VK_8, KeyEvent.VK_9,KeyEvent.VK_0, KeyEvent.VK_TAB, KeyEvent.VK_H};
-    public static int key;
-	
-	public static void update(boolean[] actionArray)
+	private ConcurrentHashMap keyBindings = new ConcurrentHashMap<Integer,Object>();
+
+	public KeyboardInput()
 	{
-		if(actionArray[0])
-			boolIsUp = true;
-		if(actionArray[1])
-			boolIsDown = true;
-		if(actionArray[2])
-			boolIsLeft = true;
-		if(actionArray[3])
-			boolIsRight = true;
-		if(actionArray[4])
-			boolIsAttack = true;
-		if(actionArray[5])
-			boolIsInteracting = true;
-		if(actionArray[6])
-		    boolIs1 = true;
-	    if(actionArray[7])
-	        boolIs2 = true;
-	    if(actionArray[8])
-	        boolIs3 = true;
-	    if(actionArray[9])
-	        boolIs4 = true;
-	    if(actionArray[10])
-	        boolIs5 = true;
-	    if(actionArray[11])
-	        boolIs6 = true;
-	    if(actionArray[12])
-	        boolIs7 = true;
-	    if(actionArray[13])
-	        boolIs8 = true;
-	    if(actionArray[14])
-	        boolIs9 = true;
-	    if(actionArray[15])
-	        boolIs0 = true;	      
-		if(actionArray[16])
-		    diagnostic = true;
-		if(actionArray[17])
-		    boolIsHeal = true;
+		System.out.println("Created Key Listener");
+		//initializes all the keys..haha, literal keys to the Keys in the Hashmap
+		//Create method object
+		//save as value
 	}
-	
-	public static void main(String[] args)
+
+	public void invoke(long window, int key, int scancode, int action, int mods)
 	{
-		Window window = new Window();
-		window.createWindow(false);
-		Window.window.addKeyListener(new KeyboardInput());
-	}
-	
-	public void keyTyped(KeyEvent e)
-	{
-		//useless, don't need
-	}
-	public void keyPressed(KeyEvent e)
-	{
-		key = e.getKeyCode();
-		for(int i = 0; i < keyArray.length; i++)
-		{
-			if(keyArray[i] == key)
-			{
-			    System.out.println(i);
-				actionArray[i] = true;
-				update(actionArray);
-				if(i <4)//all arrow keys have indices less than 4
-					boolIsMoving = true;
-				break;
-			}
-		}
-       System.out.println("keyPressed: " + key);
-	 }
-	   
-	public void keyReleased(KeyEvent e)
-	{
-		key = e.getKeyCode();
-		for(int i = 0; i < keyArray.length; i++)
-		{
-			if(keyArray[i] == key)
-			{
-				actionArray[i] = false;
-				//update();
-				System.out.println(boolIsAttack);
-				break;
-			}
-		}
-		System.out.println("keyReleased: " + key);
+		System.out.println(key);
+		//keyBindings.get(key).invoke();
 	}
 }
--- LegacyDungeon.java
+++ LegacyDungeon.java
@@ -13,7 +13,7 @@ import static org.lwjgl.system.MemoryUtil.*;
 
 public class LegacyDungeon
 {
-	private static long window;
+	private static long window;//the reference to the actual window
 	private static void init()
 	{//initialize everything
 		//Creating window with GLFW
@@ -22,15 +22,15 @@ public class LegacyDungeon
 		glfwWindowHint(GLFW_VISIBLE, GL_FALSE);//makes Window invisible for now
 		glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);//no resizing
 		//user profile
-		int width = 500;
-		int height = 500;
-		window = glfwCreateWindow(width, height, "Legacy Dungeon", NULL, NULL);
+		int windowWidth = 500;
+		int windowHeight = 500;
+		window = glfwCreateWindow(windowWidth, windowHeight, "Legacy Dungeon", NULL, NULL);//this creates the Window
 		if(window == NULL)
 			throw new RuntimeException("Failed to create window..Aborting!");
 		ByteBuffer vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-		glfwSetWindowPos(window,(GLFWvidmode.width(vidmode)-width)/2,(GLFWvidmode.height(vidmode)-height)/2);
+		glfwSetWindowPos(window,(GLFWvidmode.width(vidmode)-windowWidth)/2,(GLFWvidmode.height(vidmode)-windowHeight)/2);
 		glfwMakeContextCurrent(window);
-		glfwSwapInterval(1);
+		glfwSwapInterval(1);//waits for 1 frame to wait before swapping buffers. 1 is recommended by GLFW bc it prevent screen tearing (=0) and doesn't lead to input latency (>1)
 		glfwSetWindowCloseCallback(window,new GLFWWindowCloseCallback(){
 			public void invoke(long callBack)
 			{
@@ -38,22 +38,57 @@ public class LegacyDungeon
 			}
 		});
 		glfwShowWindow(window);
+		KeyboardInput key = new KeyboardInput();
+		glfwSetKeyCallback(window, key);
+		
+	}
+	/*
+	These two methods (createWindow and loadSaveFile) will be called concurrently from init().
+	createWindow will create the window.
+	loadSaveFile will read the save file.
+	The introAnimation is called by createWindow in the scenario 
+	*/
+	private static void createWindow()
+	{
+
+	}
+	private static void introAnimation()
+	{
+
+	}
+	private static void loadSaveFile()
+	{
+
 	}
 	private static void loop()
 	{
+		/*
+		In the game loop, there will be 2-3 primary threads that we'll be in charge of (GLFW will be managing I/O). 
+		Thread 1 is the normal thread where everything in the game happens
+		Thread 2 is the save thread where it constantly updates the save file whenever something significant happens
+		Thread 3 is the AI thread for monsters, bosses, etc.
+		*/
 		GLContext.createFromCurrent();
 		glClearColor(1,1,1,1);
 		while(true)
 		{
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 			glfwSwapBuffers(window);
-			glfwPollEvents();//polls events
+			glfwPollEvents();//polls events continuously
 		}
 	}
 	private static void term()
 	{//terminate everything
+
 		glfwTerminate();
 	}
+
+	//Creating key input with GLFW. This is the GLOBAL key input. Do we need a global key input?
+	private static void invoke(long window, int key, int scancode, int action, int mods)
+	{
+
+	}
+
 	//Global variables here
 	private boolean inGame;
 	public static void main(String[] args)
